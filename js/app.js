fd7aa7b94c407c37ac0bc065b38caa10f31bfaac
LucasCapri <capriatalucas@gmail.com>
2020-08-23 10:46:27 -0300

    Falta validar datos de temporizador

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 import { hamburguesa } from "./hamburguesa.js";
 import { reloj, alarma } from "./reloj.js";
 import { moverBola, shortcuts } from "./teclado.js";
+import { tempo } from "./temporizador.js";
 const d = document;
-console.log("g");
 d.addEventListener("DOMContentLoaded", e => {
 	hamburguesa("#boton-flotante", ".menu", ".menu a");
 	reloj(".reloj", "#activar-reloj", "#desactivar-reloj");
 	alarma("#activar-alarma", "#desactivar-alarma");
 	moverBola(".bola", ".contenedor-bola");
 	shortcuts(".encabezado");
+	tempo("#temporizador", "#boton-temporizador", "#cancelar-temporizador");
 });
--- js/temporizador.js
+++ js/temporizador.js
@@ -1,8 +1,54 @@
 const d = document;
-export function tempo(temporizador) {
-	d.addEventListener();
+var regresiva;
+export function tempo(temporizador, btnTemporizador, cancelarTemporizador) {
+	d.addEventListener("click", e => {
+		if (
+			e.target.matches(btnTemporizador) ||
+			e.target.matches(`${btnTemporizador} *`)
+		) {
+			let fechaFinal = desarmarFecha(
+				prompt("Ingrese una fecha en formato dd/mm/aaaa")
+			);
+			regresiva = setInterval(() => {
+				let fechaAMostrar = restaHoyFecha(new Date(), fechaFinal);
+				d.querySelector(
+					temporizador
+				).innerText = `Dias: ${fechaAMostrar.dias}; Horas: ${fechaAMostrar.horas}; Minutos: ${fechaAMostrar.minutos}; Segundo: ${fechaAMostrar.segundos}`;
+			}, 1000);
+		}
+		if (
+			e.target.matches(cancelarTemporizador) ||
+			e.target.matches(`${cancelarTemporizador} *`)
+		) {
+			clearInterval(regresiva);
+			d.querySelector(
+				temporizador
+			).innerText = `Dias: ? ; Horas: ? ; Minutos: ? ; Segundo: ?`;
+		}
+	});
 }
 
+function restaHoyFecha(hoy, fechaFinal) {
+	let difMs = fechaFinal - hoy;
+	return convertMS(difMs);
+}
+
+function convertMS(milliseconds) {
+	let dias, horas, minutos, segundos;
+	segundos = Math.floor(milliseconds / 1000);
+	minutos = Math.floor(segundos / 60);
+	segundos = segundos % 60;
+	horas = Math.floor(minutos / 60);
+	minutos = minutos % 60;
+	dias = Math.floor(horas / 24);
+	horas = horas % 24;
+	return {
+		dias,
+		horas,
+		minutos,
+		segundos,
+	};
+}
 function desarmarFecha(fecha) {
 	//Sabemos que fecha es de formato "dd/mm/aaaa"
 	const arrayFecha = fecha.split("/");
